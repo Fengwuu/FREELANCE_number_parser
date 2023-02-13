301257b6ce8ee44b39cca41ec3de7e3bc018909f
Fengwuu <filipp.pustovoitenko.work@gmail.com>
2023-02-13 21:39:43 +0200

    allowed selection of numbers and city

--- main.py
+++ main.py
@@ -3,21 +3,26 @@ from bs4 import BeautifulSoup
 import re
 
 print('Hello! Here you can scan indian phone numbers. All what I need - city and number.')
-a = 'Delhi'
+
+city = input('Enter Indian city, that you need.') #asking for some information
 file = open("numbers.txt", "w")
-for x in range (7000, 7100): # numbers
+first_number = int(input('Input first number, that you need (0+)'))
+last_number = int(input('Input last number, that you need (9999-)'))
+
+for x in range (first_number, last_number+1): # numbers, +1 because range decrement last integer.
     r = requests.get('https://mobilenumbertracker.com/mobilenumberlocation/'+str(x))
 
     soup = BeautifulSoup(r.text, 'html.parser')
+
     divs = soup.find_all('p') # information in p html tag
-    words = re.findall(r'Delhi', str(divs)) # sorting by city
+    words = re.findall(r'{}'.format(city), str(divs)) # sorting by city
     
     try:
-        print('City: ' + words[0] + ' Number: ' + str(x)) 
-        file.write("Number: "  + str(x) +  ", City: " + words[0] + '\n')
-        
+        print('City: ' + words[0] + ' Number: ' + str(x)) # try is required, necessary because there are numbers that do not...
+        file.write("Number: "  + str(x) +  ", City: " + words[0] + '\n')  #...  allow tracking information, which breaks the script
     except:
         print (str(x) + ' Not found in this city!')
       
+
 file.close()
 print('Program finished! Look at numbers.txt in this directory. Bye-Bye')
